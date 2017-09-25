557677eda6ebfa653b9f355f15a7aa380a81b3da
Dennis <dennis@thehumanswissarmyknife.de>
2017-09-25 15:39:13 +0000

    small changes

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
 const path = require('path');
 const express = require('express');
 
-const publicPath = path.join(__dirname + '/../public');
-const port = process.env.PORT || 3000; 
-
-
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express();
-
-app.listen(port, () => {
-	console.log(`Server is up and running. listening to port ${port}`);
-});
 
 app.use(express.static(publicPath));
 
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
